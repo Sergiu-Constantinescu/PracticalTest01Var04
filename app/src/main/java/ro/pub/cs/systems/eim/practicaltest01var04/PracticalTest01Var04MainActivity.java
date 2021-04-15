3049aadf9c54017b5ed6db90f144b1ea5a014e36
Estohan <sergiuconstantinescu7@yahoo.com>
2021-04-15 09:58:06 +0300

    B1

--- app/src/main/java/ro/pub/cs/systems/eim/practicaltest01var04/PracticalTest01Var04MainActivity.java
+++ app/src/main/java/ro/pub/cs/systems/eim/practicaltest01var04/PracticalTest01Var04MainActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -21,11 +23,25 @@ public class PracticalTest01Var04MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Toast.makeText(getApplicationContext(), "Displaying info!", Toast.LENGTH_SHORT).show();
+                EditText editText_name = findViewById(R.id.edit_text_name);
+                EditText editText_group = findViewById(R.id.edit_text_group);
+                CheckBox check_name =  (CheckBox) findViewById(R.id.checkbox_name);
+                CheckBox check_group =  (CheckBox) findViewById(R.id.checkbox_group);
+                String group = editText_group.getText().toString();
+                String name = editText_name.getText().toString();
+
+                if(!check_name.isChecked()) {
+                    name = "";
+                }
+
+                if(!check_group.isChecked()) {
+                    group = "";
+                }
 
                 setContentView(R.layout.activity_practical_test01_var04_main);
 
                 TextView tv1 = (TextView)findViewById(R.id.text_info);
-                tv1.setText("Sergiu Constantinescu 343C4");
+                tv1.setText(name + " "  + group);
             }
         });
     }
